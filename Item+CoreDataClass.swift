4beee6e54248ec3294640a61d99cd83395a09576
Miftah. Jihad <jihad.strange@gmail.com>
2021-07-26 00:50:32 +0800

    [ADD] DB entity (#13)

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  iWarung
+//
+//  Created by Miftahul Jihad on 25/07/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ iWarung/Controllers/ProductCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class ProductCell: UITableViewCell {
+    @IBOutlet weak var namaLabel: UILabel!
+    @IBOutlet weak var hargaLabel: UILabel!
+    
+}
