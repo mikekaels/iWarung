6940bd717eacb5c9f34dec99d7660f046e4b5862
kaelsihombing <sntmcl1@gmail.com>
2021-07-26 00:06:56 +0700

    Daftar Produk

---
+++ iWarung/Controllers/DaftarProdukViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DaftarProdukViewController.swift
+//  iWarung
+//
+//  Created by Santo Michael Sihombing on 26/07/21.
+//
+
+import UIKit
+
+class DaftarProdukViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Daftar Produk"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
