c8f00b78f853a2dcb228eca1e3691e6fb9096208
Miftah. Jihad <jihad.strange@gmail.com>
2021-07-28 23:00:56 +0800

    Feature/add product (#25)
    * add popup
    
    * add product form
    
    * continue add product
    
    Co-authored-by: kaelsihombing <sntmcl1@gmail.com>

--- iWarung/Controllers/TambahProdukFormViewController.swift
+++ iWarung/Controllers/TambahProdukFormViewController.swift
@@ -11,12 +11,18 @@ import CoreData
 class TambahProdukFormViewController: UIViewController {
 
     @IBOutlet weak var addProdItem: UIButton!
+    @IBOutlet weak var deleteProdukButton: UIButton!
     @IBOutlet weak var idTF: UITextField!
     @IBOutlet weak var namaTF: UITextField!
+    @IBOutlet weak var hargamodalTF: UITextField!
     @IBOutlet weak var hargaTF: UITextField!
+    @IBOutlet weak var stockTF: UITextField!
     @IBOutlet weak var deskTF: UITextView!
     @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var imageThumnail: UIImageView!
+    
+    var selectedItem: Item? = nil
+    var scanningBarcode: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +30,21 @@ class TambahProdukFormViewController: UIViewController {
         
         imageThumnail.cornerRadius()
         
-        
         addProdItem.cornerRadius(width: 10, height: 4)
         addProdItem.addGradient()
+        deleteProdukButton.cornerRadius(width: 10, height: 4)
+        deleteProdukButton.cornerRadius()
         
-        idTF.text = "1878437298"
+        
+        if (selectedItem != nil) {
+            idTF.text = "\(String(describing: selectedItem?.id))"
+            namaTF.text = selectedItem?.nama
+            deskTF.text = selectedItem?.deskripsi
+            hargaTF.text = selectedItem?.harga
+            
+        } else {
+            idTF.text = scanningBarcode
+        }
         
         self.setupHideKeyboardOnTap()
     }
@@ -51,6 +67,54 @@ class TambahProdukFormViewController: UIViewController {
     }
     
     @IBAction func addProduct(_ sender: Any){
+        print("Saving...")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        if (selectedItem == nil) {
+            let entity = NSEntityDescription.entity(forEntityName: "Item", in: context)
+            let newProduct = Item(entity: entity!, insertInto: context)
+            
+            newProduct.imageD = imageThumnail.image?.pngData()
+            newProduct.id = Int32(truncating: productList.count as NSNumber)
+            newProduct.nama = namaTF.text
+            newProduct.harga = hargaTF.text
+            newProduct.deskripsi = deskTF.text
+            
+            do {
+                try context.save()
+                productList.append(newProduct)
+                dismiss(animated: true)
+    //            navigationController?.popViewController(animated: true)
+                print("Saved")
+            } catch {
+                print("error saving dataa")
+            }
+            
+            // edit
+        } else {
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
+            do {
+                let results:NSArray = try context.fetch(request) as NSArray
+                for result in results {
+                    let item = result as! Item
+                    if (item == selectedItem) {
+                        item.id = Int32(truncating: productList.count as NSNumber)
+                        item.nama = namaTF.text
+                        item.harga = hargaTF.text
+                        item.deskripsi = deskTF.text
+                        item.imageD = imageThumnail.image?.pngData()
+                        try context.save()
+                        dismiss(animated: true)
+                    }
+                }
+            } catch {
+                print("Fetch failed")
+            }
+        }
+    }
+    
+    @IBAction func deleteProduk(_ sender: Any) {
         
     }
     
@@ -60,7 +124,7 @@ class TambahProdukFormViewController: UIViewController {
 
 }
 
-//UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
 extension TambahProdukFormViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func takePhotoWithCamera() {
