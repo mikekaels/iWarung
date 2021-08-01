32f689432142eed2c0a756dff68aed5fb4628ba5
kaelsihombing <sntmcl1@gmail.com>
2021-08-01 09:39:25 +0700

    bug fixed

--- iWarung/Controllers/ViewController.swift
+++ iWarung/Controllers/ViewController.swift
@@ -441,17 +441,3 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         }
     }
 }
-
-class PassThroughView: UIView {
-    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-        for subview in subviews {
-            if !subview.isHidden && subview.isUserInteractionEnabled && subview.point(inside: convert(point, to: subview), with: event) {
-                return true
-            }
-            break
-        default:
-            break
-        }
-        return false
-    }
-}
