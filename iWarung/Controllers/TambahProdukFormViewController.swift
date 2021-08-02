ae7ea8fc050f3b22c14a3f7a67bea7cd1c8cd197
jiad <jihad.strange@gmail.com>
2021-08-02 23:04:59 +0800

    label simpan perubahan

--- iWarung/Controllers/TambahProdukFormViewController.swift
+++ iWarung/Controllers/TambahProdukFormViewController.swift
@@ -44,7 +44,7 @@ class TambahProdukFormViewController: UIViewController {
             kadaluwarsaPicker.date = (selectedItem?.exp_date!)!
             imageThumnail.image = UIImage(data: (selectedItem?.image_data)!)
             
-            addProdItem.setTitle("Simpan Perubahan", for: UIControl.State.highlighted)
+            addProdItem.setTitle("Simpan Perubahan", for: UIControl.State.normal)
             
         } else {
             deleteProdukButton.isHidden = true
