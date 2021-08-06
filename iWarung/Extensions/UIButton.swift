3857092c84479a1a6f9739e85baa2464bcb5a3ea
Santo Michael Sihombing <59433403+kaelsihombing@users.noreply.github.com>
2021-08-07 06:04:18 +0700

    Update UI Daftar Produk, Search Functionality, Redesign Filter and Sorting (#43)

--- iWarung/Extensions/UIButton.swift
+++ iWarung/Extensions/UIButton.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 extension UIButton{
-
+    func createSegmentedControlButton(setTitle to: String)-> UIButton {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(to, for: .normal)
+        button.setTitleColor(K.greyLight, for: .normal)
+        button.titleLabel?.font = K.segmentedButton
+        
+        let buttonTitleSize = (to as NSString).size(withAttributes: [NSAttributedString.Key.font : K.segmentedButton])
+        print(to,": ",buttonTitleSize.width)
+        
+        button.widthAnchor.constraint(equalToConstant: CGFloat(buttonTitleSize.width + 20)).isActive = true
+        button.heightAnchor.constraint(equalToConstant: CGFloat(buttonTitleSize.height)).isActive = true
+        
+        return button
+    }
 }
-
